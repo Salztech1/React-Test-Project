f630c6109c5469d9eb30fb46ab945bc998793f42
salome <salomenjinda0@gmail.com>
2024-10-06 10:22:55 +0100

    made update on file positions

--- src/components/Navigation/SideNav.jsx
+++ src/components/Navigation/SideNav.jsx
@@ -1,29 +1,29 @@
-import React from 'react';
-import SideNavItem from './SideNavItem'; 
-import SuggestedList from '../../Suggestions/SuggestedList'; 
-import { navItems } from '../Data/Data';
+import React from "react";
+import SideNavItem from "./SideNavItem";
+import SuggestedList from "../Suggestions/SuggestedList";
+import { navItems } from "../Data/Data";
 
 const SideNav = () => {
-    return (
-      <div>
-        <nav className="navbar bg-social-bg-200 mt-5 ml-3 rounded-md">
-          <ul className="nav-links ml-6 p-4">
-
-            {navItems.map((navItem,index)=>{
-              return  <SideNavItem 
+  return (
+    <div>
+      <nav className="navbar bg-social-bg-200 mt-5 ml-3 rounded-md">
+        <ul className="nav-links ml-6 p-4">
+          {navItems.map((navItem, index) => {
+            return (
+              <SideNavItem
                 key={index}
                 to={navItem.to}
                 icon={navItem.icon}
                 label={navItem.label}
-                />;
-            })}
-
-          </ul>
-          <hr className="w-48 ml-5 border-gray-500" />
-          <SuggestedList />
-        </nav>
-      </div>
-    );
+              />
+            );
+          })}
+        </ul>
+        <hr className="w-48 ml-5 border-gray-500" />
+        <SuggestedList />
+      </nav>
+    </div>
+  );
 };
 
 export default SideNav;
--- src/components/Suggestions/SuggestedList.jsx
+++ src/components/Suggestions/SuggestedList.jsx
@@ -1,12 +1,11 @@
-import React from 'react';
-import { suggestedPages } from '../components/Data/Data';
+import React from "react";
+import { suggestedPages } from "../Data/Data";
 
 const SuggestedList = () => {
-  
   return (
     <div className="mt-3">
-      <h6 className='ml-5 text-gray-400 text-sm'> PAGES YOU LIKE</h6>
-      <ul className='mt-2 p-5 text-xs'>
+      <h6 className="ml-5 text-gray-400 text-sm"> PAGES YOU LIKE</h6>
+      <ul className="mt-2 p-5 text-xs">
         {suggestedPages.map((page) => (
           <li key={page.id} className=" flex items-center mb-4">
             <img
